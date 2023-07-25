de2f626fc612dfe017320c56a9c1cc297ec42bcb
Vikas Kumar <vikas.gta@gmail.com>
2023-07-26 01:00:45 +0530

    Update 26.RemoveDuplicatesFromSortedArray.java

--- 26.RemoveDuplicatesFromSortedArray.java
+++ 26.RemoveDuplicatesFromSortedArray.java
@@ -1,30 +1,27 @@
 public class RemoveDuplicates {
-    private static int removeDuplicates(int arr[]) {
-        if (arr.length == 0) {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
             return 0;
         }
-        int size = arr.length;
+        int size = nums.length;
         int uniqueElementIndex = 0;
         for (int i = 0; i< size-1; i++) {
-            if (arr[i] != arr[i+1]) {
-                arr[uniqueElementIndex++] = arr[i];
+            if (nums[i] != nums[i+1]) {
+                nums[uniqueElementIndex++] = nums[i];
             }
         }
-        arr[uniqueElementIndex++] = arr[size-1];
+        nums[uniqueElementIndex++] = nums[size-1];
         return uniqueElementIndex;
-    }
-    
-    public static void main(String []args) {
-        int arr1[] = {1,2,3,4};
-        System.out.println(removeDuplicates(arr1));
-        int arr2[] = {0,0,1,1,1,2,2,3,3,4};
-        System.out.println(removeDuplicates(arr2));
-        int arr3[] = {0,0,0,0,0,0,0};
-        System.out.println(removeDuplicates(arr3));
     }
 }
 
-/**
+/*
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+
 As array is sorted we can compare current element with the next then copy it only if both are not equal
 Time Complexity: O(N) N is length of array
 Space Complexity:O(1)
