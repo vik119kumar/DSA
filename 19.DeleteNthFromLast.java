1b206958d63f40d65771895a91cab4c70da8c99f
Vikas Kumar <vikas.gta@gmail.com>
2023-07-26 00:36:57 +0530

    Update 19.DeleteNthFromLast.java

--- 19.DeleteNthFromLast.java
+++ 19.DeleteNthFromLast.java
@@ -1,41 +1,19 @@
 public class DeleteNthFromLast {
-    class Node {
-        int data;
-        Node next;
-        Node(int data)
-        {
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    Node head;
-
-    public void push(int data) {
-        Node new_node = new Node(data);
-        new_node.next = head;
-        head = new_node;
-    }
-
-    public void show() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + "->");
-            temp = temp.next;
-        }
-        System.out.println("X");
-    }
-
-    public void deleteNthNodeFromEnd(int n) {
-        Node first = head;
-        Node second = head;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
 
         for (int i = 0; i < n; i++) {
             first = first.next;
+            if (first == null) {
+                return null;
+            }
         }
 
         if (first == null) {
-            return;
+         return dummy.next;
         }
 
         while (first.next != null) {
@@ -43,23 +21,7 @@ public class DeleteNthFromLast {
             second = second.next;
         }
         second.next = second.next.next;
-    }
-
-    public static void main(String args[]) {
-        DeleteNthFromLast obj = new DeleteNthFromLast();
-        obj.push(5);
-        obj.push(4);
-        obj.push(3);
-        obj.push(2);
-        obj.push(1);
-
-        System.out.println("List:");
-        obj.show();
-
-        obj.deleteNthNodeFromEnd(2);
-
-        System.out.println("Deleted List:");
-        obj.show();
+        return dummy.next;
     }
 }
 /*
